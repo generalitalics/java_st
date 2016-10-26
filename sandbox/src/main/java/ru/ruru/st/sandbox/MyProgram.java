3d21e81cc69bc61b3ab892fbd66fc1923571231f
generalitalics <generalitalics@mail.ru>
2016-10-26 18:17:32 +0300

    Дозаливка

--- sandbox/src/main/java/ru/ruru/st/sandbox/MyProgram.java
+++ sandbox/src/main/java/ru/ruru/st/sandbox/MyProgram.java
@@ -1,8 +1,7 @@
 package ru.ruru.st.sandbox;
 
 public class MyProgram {
-	public static void main(String[] args) {
-
-		System.out.println("Hello-Hello, dears!");
-	}
+    public static void main(String[] args) {
+        System.out.println("Hello-Hello, dears!");
+    }
 }
