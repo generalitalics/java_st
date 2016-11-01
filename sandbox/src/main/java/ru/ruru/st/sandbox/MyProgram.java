07df81909eaf400a1ed55f3ebcea0edb46222612
generalitalics <generalitalics@mail.ru>
2016-11-01 19:30:53 +0300

    Не значительное изменение

--- sandbox/src/main/java/ru/ruru/st/sandbox/MyProgram.java
+++ sandbox/src/main/java/ru/ruru/st/sandbox/MyProgram.java
@@ -1,6 +1,6 @@
 package ru.ruru.st.sandbox;
 
-public class MyProgram {
+public class    MyProgram {
     public static void main(String[] args) {
         System.out.println("Hello-Hello, dears!");
     }
