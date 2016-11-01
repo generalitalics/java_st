9c89b070fd2546ad61dcd4e9b0c1e9fe71056d8a
generalitalics <generalitalics@mail.ru>
2016-11-02 00:40:36 +0300

    Добавлены несколько функций

--- sandbox/src/main/java/ru/ruru/st/sandbox/MyProgram.java
+++ sandbox/src/main/java/ru/ruru/st/sandbox/MyProgram.java
@@ -1,7 +1,24 @@
 package ru.ruru.st.sandbox;
 
-public class    MyProgram {
+public class MyProgram {
     public static void main(String[] args) {
-        System.out.println("Hello-Hello, dears!");
+        hello("Max");
+        System.out.println("Площадь " + area(4));
+        System.out.println("Площадь прямоугольника " + area(4, 6));
+        double a = 1.0;
+        double s = a + a;
+        System.out.println("Вот сумма " + a + " + " + a + " = " + s);
+    }
+
+    public static void hello(String somebody) {
+        System.out.println("Hello-Hello, "+ somebody +"!");
+    }
+
+    public static double area(double a) {
+        return a*a;
+    }
+
+    public static double area(double a, double b) {
+        return a*b;
     }
 }
