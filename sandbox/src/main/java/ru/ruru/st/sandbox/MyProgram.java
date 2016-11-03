eee0b63d759baf171af0f8d373babb2276f0861e
generalitalics <generalitalics@mail.ru>
2016-11-03 23:30:54 +0300

    добавление класса Point и функции distance

--- sandbox/src/main/java/ru/ruru/st/sandbox/MyProgram.java
+++ sandbox/src/main/java/ru/ruru/st/sandbox/MyProgram.java
@@ -13,10 +13,18 @@ public class MyProgram {
         double a = 1.0;
         double sum = a + a;
         System.out.println("Вот сумма " + a + " + " + a + " = " + sum);
+
+        Point p1 = new Point(1, 3);
+        Point p2 = new Point(5, 7);
+        System.out.println("Расстояние между точками = " + distance(p1, p2));
     }
 
     public static void hello(String somebody) {
         System.out.println("Hello-Hello, "+ somebody +"!");
+    }
+    /*Задание2*/
+    public static double distance(Point p1,Point p2) {
+        return Math.sqrt((p1.x - p2.x) * (p1.x - p2.x) + (p1.y - p2.y) * (p1.y - p2.y));
     }
 
 }
---
+++ sandbox/src/main/java/ru/ruru/st/sandbox/Point.java
@@ -0,0 +1,14 @@
+package ru.ruru.st.sandbox;
+
+/**
+ * Created by Dospeh on 03.11.2016.
+ */
+public class Point {
+    public double x;
+    public double y;
+
+    public Point(double x, double y) {
+        this.x = x;
+        this.y = y;
+    }
+}
