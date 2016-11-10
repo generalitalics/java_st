e5abb91560b2d095f3d74b3f056e34f825633da5
generalitalics <generalitalics@mail.ru>
2016-11-10 18:16:50 +0300

    Создал первый тест для addressbook/contact  и провел рефакторинг

---
+++ addressbook-web-tests/src/test/java/ru/ruru/st/adressbook/ContactData.java
@@ -0,0 +1,31 @@
+package ru.ruru.st.adressbook;
+
+public class ContactData {
+    private final String name;
+    private final String lastname;
+    private final String mnumber;
+    private final String email;
+
+    public ContactData(String name, String lastname, String mnumber, String email) {
+        this.name = name;
+        this.lastname = lastname;
+        this.mnumber = mnumber;
+        this.email = email;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public String getMnumber() {
+        return mnumber;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+}
