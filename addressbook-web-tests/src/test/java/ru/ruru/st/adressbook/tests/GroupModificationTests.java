66b8ea4b5126f9be9cd5507a8aaf2552ef4f6cb5
generalitalics <generalitalics@mail.ru>
2016-11-18 13:53:20 +0300

    Добавлен тест GroupModification

---
+++ addressbook-web-tests/src/test/java/ru/ruru/st/adressbook/tests/GroupModificationTests.java
@@ -0,0 +1,20 @@
+package ru.ruru.st.adressbook.tests;
+
+import org.testng.annotations.Test;
+import ru.ruru.st.adressbook.model.GroupData;
+
+/**
+ * Created by m.shoshin on 18.11.2016.
+ */
+public class GroupModificationTests extends TestBase{
+
+    @Test
+    public void testGroupModification() {
+        app.getNavigationHelper().gotoGroupPage();
+        app.getGroupHelper().selectGroup();
+        app.getGroupHelper().GroupModification();
+        app.getGroupHelper().fillGroupForm(new GroupData("test1", "test2", "test3"));
+        app.getGroupHelper().submitGroupModification();
+        app.getGroupHelper().returnToGroupPage();
+    }
+}
