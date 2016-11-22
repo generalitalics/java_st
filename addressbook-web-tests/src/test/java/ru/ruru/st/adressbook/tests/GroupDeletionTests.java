5863c083125e8b9515bc02c37dcd535672e33be3
generalitalics <generalitalics@mail.ru>
2016-11-22 14:11:51 +0300

    Реализована проверка и обеспечение выполнения предусловий

--- addressbook-web-tests/src/test/java/ru/ruru/st/adressbook/tests/GroupDeletionTests.java
+++ addressbook-web-tests/src/test/java/ru/ruru/st/adressbook/tests/GroupDeletionTests.java
@@ -1,12 +1,16 @@
 package ru.ruru.st.adressbook.tests;
 
 import org.testng.annotations.Test;
+import ru.ruru.st.adressbook.model.GroupData;
 
 public class GroupDeletionTests extends TestBase {
 
     @Test
     public void testGroupDeletion() {
         app.getNavigationHelper().gotoGroupPage();
+        if (!app.getGroupHelper().isThereAGroup()) {
+            app.getGroupHelper().createGroup(new GroupData("test1", null, null));
+        }
         app.getGroupHelper().selectGroup();
         app.getGroupHelper().deleteSelectedGroups();
         app.getGroupHelper().returnToGroupPage();
