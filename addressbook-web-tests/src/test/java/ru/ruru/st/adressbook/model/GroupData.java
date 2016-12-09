e759d221df99193435056baf84cf4f4032256941
generalitalics <generalitalics@mail.ru>
2016-12-09 18:54:48 +0300

    Изменение кода групп в стиле fluent-интерфейсов

--- addressbook-web-tests/src/test/java/ru/ruru/st/adressbook/model/GroupData.java
+++ addressbook-web-tests/src/test/java/ru/ruru/st/adressbook/model/GroupData.java
@@ -2,19 +2,10 @@ package ru.ruru.st.adressbook.model;
 
 public class GroupData {
 
-    private int id;
-    private final String name;
-    private final String header;
-    private final String footer;
-
-
-    public GroupData(String name, String header, String footer) {
-        this.id = Integer.MAX_VALUE;
-        this.name = name;
-        this.header = header;
-        this.footer = footer;
-    }
-
+    private int id = Integer.MAX_VALUE;
+    private String name;
+    private String header;
+    private String footer;
 
     public int getId() {
         return id;
@@ -32,8 +23,24 @@ public class GroupData {
         return footer;
     }
 
-    public void setId(int id) {
+    public GroupData withId(int id) {
         this.id = id;
+        return this;
+    }
+
+    public GroupData withName(String name) {
+        this.name = name;
+        return this;
+    }
+
+    public GroupData withHeader(String header) {
+        this.header = header;
+        return this;
+    }
+
+    public GroupData withFooter(String footer) {
+        this.footer = footer;
+        return this;
     }
 
     @Override
@@ -51,15 +58,6 @@ public class GroupData {
     public int hashCode() {
         return name != null ? name.hashCode() : 0;
     }
-
-    public GroupData(int id, String name, String header, String footer) {
-        this.id = id;
-        this.name = name;
-        this.header = header;
-        this.footer = footer;
-
-    }
-
 
     @Override
     public String toString() {
