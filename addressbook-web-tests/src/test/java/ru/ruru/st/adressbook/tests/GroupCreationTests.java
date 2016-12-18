3b9e87516a1d4aa3c3661f861570000ce9644486
generalitalics <generalitalics@mail.ru>
2016-12-18 15:32:22 +0300

    Создал генератор тестовых данных + изменил тест создания групп с подгрузкой данных из файла

--- addressbook-web-tests/src/test/java/ru/ruru/st/adressbook/tests/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/ruru/st/adressbook/tests/GroupCreationTests.java
@@ -1,26 +1,44 @@
 package ru.ruru.st.adressbook.tests;
 
+import org.apache.xpath.SourceTree;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 import ru.ruru.st.adressbook.model.GroupData;
 import ru.ruru.st.adressbook.model.Groups;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
 
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
 
 public class GroupCreationTests extends TestBase {
 
-    @Test
-    public void testGroupCreation() {
-        app.goTo().groupPage();
-        Groups before = app.group().all();
-        GroupData group = new GroupData().withName("test2");
-        app.group().create(group);
-        assertThat(app.group().count(), equalTo(before.size() + 1));
-        Groups after = app.group().all();
-        assertThat(after, equalTo(
-                before.withAdded(group.withId(after.stream().mapToInt((g) -> g.getId()).max().getAsInt()))));
+    @DataProvider
+    public Iterator<Object[]> validGroups() throws IOException {
+        List<Object[]> list = new ArrayList<Object[]>();
+        BufferedReader reader = new BufferedReader(new FileReader(new File("src/test/resources/groups.csv")));
+        String line = reader.readLine();
+        while (line != null) {
+            String[] split = line.split(";");
+            list.add(new Object[] {new GroupData().withName(split[0]).withHeader(split[1]).withFooter(split[2])});
+            line = reader.readLine();
+        }
+        return  list.iterator();
     }
+    @Test(dataProvider = "validGroups")
+    public void testGroupCreation(GroupData group) {
+            app.goTo().groupPage();
+            Groups before = app.group().all();
+            app.group().create(group);
+            assertThat(app.group().count(), equalTo(before.size() + 1));
+            Groups after = app.group().all();
+            assertThat(after, equalTo(
+                    before.withAdded(group.withId(after.stream().mapToInt((g) -> g.getId()).max().getAsInt()))));
 
+    }
     @Test
     public void testBedGroupCreation() {
         app.goTo().groupPage();
