ee666c66c5448710a4d1308c0b974e81e9794559
generalitalics <generalitalics@mail.ru>
2017-01-24 18:49:45 +0300

    Создал mantis

---
+++ mantis-tests/src/test/java/ru/ruru/st/mantis/tests/TestBase.java
@@ -0,0 +1,28 @@
+package ru.ruru.st.mantis.tests;
+
+import org.openqa.selenium.remote.BrowserType;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeSuite;
+import ru.ruru.st.mantis.appmanager.ApplicationManager;
+
+/**
+ * Created by m.shoshin on 11.11.2016.
+ */
+public class TestBase {
+
+
+    protected static final ApplicationManager app
+            = new ApplicationManager(System.getProperty("browser", BrowserType.CHROME));
+
+    @BeforeSuite
+    public void setUp() throws Exception {
+        app.init();
+    }
+
+    @AfterSuite(alwaysRun = true)
+    public void tearDown() {
+        app.stop();
+    }
+
+}
+
